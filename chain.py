e1113b47f630a994f985322b1ce7df3ec4e2e151
yifei yan <yanyiphei@gmail.com>
2024-10-07 13:32:58 -0700

    linting

--- chain.py
+++ chain.py
@@ -1,18 +1,24 @@
 class NodeSchema:
-
-    def __init__(self, node_prompt, tool_fns, input_pydantic_model, state_pydantic_model):
+    def __init__(
+        self, node_prompt, tool_fns, input_pydantic_model, state_pydantic_model
+    ):
         self.node_prompt = node_prompt
         self.tool_fns = tool_fns
         self.input_pydantic_model = input_pydantic_model
         self.state_pydantic_model = state_pydantic_model
 
     def run(self, input):
-
         if input is not None:
             assert isinstance(input, self.input_pydantic_model)
 
         self.state = self.state_pydantic_model()
-        self.prompt = self.generate_system_prompt(self.input_pydantic_model is not None, node_prompt=self.node_prompt,node_input = self.input.model_dump_json() if self.input_pydantic_model is not None else None)
+        self.prompt = self.generate_system_prompt(
+            self.input_pydantic_model is not None,
+            node_prompt=self.node_prompt,
+            node_input=self.input.model_dump_json()
+            if self.input_pydantic_model is not None
+            else None,
+        )
 
     def generate_system_prompt(self, has_input, kwargs):
         NODE_PROMPT = """
@@ -38,8 +44,8 @@ class NodeSchema:
             accomplishing the user input. When in doubt, use the function/s. In conjunction, you must update a state object whenever possible.
             The state update function is update_state and getting the state function is get_state.
             You cannot proceed to the next stage without updating the state."""
-        
-        return NODE_PROMPT.format(**kwargs)            
+
+        return NODE_PROMPT.format(**kwargs)
 
     def update_state(self, state_update):
         self.state = self.state.model_copy(update=state_update)
@@ -47,25 +53,36 @@ class NodeSchema:
     def get_state(self):
         return self.state.model_dump_json()
 
+
 class EdgeSchema:
-    def __init__(self, from_node_schema, to_node_schema, state_condition_fn, new_input_from_state_fn):
+    def __init__(
+        self,
+        from_node_schema,
+        to_node_schema,
+        state_condition_fn,
+        new_input_from_state_fn,
+    ):
         self.from_node_schema = from_node_schema
         self.to_node_schema = to_node_schema
         self.state_condition_fn = state_condition_fn
         self.new_input_from_state_fn = new_input_from_state_fn
-    
+
     def check_state_condition(self, state):
         return self.state_condition_fn(state)
-    
+
+
+from pydantic import BaseModel, Field
 
 ## Chain ##
 from db_functions import Order
-from pydantic import BaseModel, Field
 
 
 class TakeOrderState(BaseModel):
     order: Order
-    has_finished_ordering: bool = Field(description="whether the customer has finished ordering. This can only be true after you have explicitly confirmed with them that they have finished ordering, by asking questions like 'Anything else?'.")
+    has_finished_ordering: bool = Field(
+        description="whether the customer has finished ordering. This can only be true after you have explicitly confirmed with them that they have finished ordering, by asking questions like 'Anything else?'."
+    )
+
 
 take_order_node_schema = NodeSchema(
     node_prompt="First, greet the customer. Then, your main job is to take their orders, which also includes answering reasonable questions about the shop & menu only"
@@ -73,22 +90,26 @@ take_order_node_schema = NodeSchema(
     " but you need to steer the conversation back to ordering after 4 back-and-forths.",
     tool_fns=[],
     input_pydantic_model=None,
-    state_pydantic_model=TakeOrderState
+    state_pydantic_model=TakeOrderState,
 )
 
+
 class ConfirmOrderState(BaseModel):
-    has_confirmed_order: bool = Field(description="whether the customer has confirmed their order")
+    has_confirmed_order: bool = Field(
+        description="whether the customer has confirmed their order"
+    )
+
 
 confirm_order_node_schema = NodeSchema(
     node_prompt="Your main job is to confirm the order with the customer. You do this by repeating the order back to them and get their confirmation.",
     tool_fns=[],
     input_pydantic_model=Order,
-    state_pydantic_model=ConfirmOrderState
+    state_pydantic_model=ConfirmOrderState,
 )
 
 take_to_confirm_edge_schema = EdgeSchema(
     from_node_schema=take_order_node_schema,
     to_node_schema=confirm_order_node_schema,
     state_condition_fn=lambda state: state.has_finished_ordering,
-    new_input_from_state_fn=lambda state: state.order
+    new_input_from_state_fn=lambda state: state.order,
 )
