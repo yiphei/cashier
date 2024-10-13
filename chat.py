2c54c7b59e3ac3d8604a173e5f996fee710d1e02
yifei yan <yanyiphei@gmail.com>
2024-10-13 10:39:13 -0700

    linting

--- chat.py
+++ chat.py
@@ -200,7 +200,7 @@ class ListIndexTracker:
         popped_idx_pos = self.idx_to_pos.pop(popped_idx)
         self.idx_to_named_idx.pop(popped_idx)
         del self.idxs[popped_idx_pos]
-        
+
         for i in range(popped_idx_pos, len(self.idxs)):
             curr_idx = self.idxs[i]
             curr_named_idx = self.idx_to_named_idx[curr_idx]
