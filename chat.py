322670ddb382ec6ad521f595caef4d1602cb21a5
yifei yan <yanyiphei@gmail.com>
2024-10-18 10:15:54 -0700

    linting

--- chat.py
+++ chat.py
@@ -20,7 +20,7 @@ from chain import (
 from db_functions import create_db_client
 from gui import remove_previous_line
 from logger import logger
-from model import Model, ModelMessage, CustomJSONEncoder
+from model import CustomJSONEncoder, Model, ModelMessage
 from model_tool_decorator import FN_NAME_TO_FN, OPENAI_TOOL_NAME_TO_TOOL_DEF
 
 # Load environment variables from .env file
