9004bd98c98e7f085a3fe0e68649e2db92447690
yifei yan <yanyiphei@gmail.com>
2024-10-26 19:26:54 -0700

    adde changes

--- chain.py
+++ chain.py
@@ -129,7 +129,7 @@ class NodeSchema:
             + "</state_guidelines>\n"
             "<tools_guidelines>\n"
             "- Minimize reliance on external knowledge. Always retrieve information from the system prompts and available tools. "
-            "If they dont provide the information you need, just say you do not know.\n"
+            "If they dont provide the information needed, just say you do not know.\n"
             "- AVOID stating/mentioning that you can/will perform an action if there are no tools (including state updates) associated with that action.\n"
             "- if you need to perform an action, you can only state to the customer that you performed it after the associated tool (including state update) calls have been successfull.\n"
             "</tools_guidelines>\n"
