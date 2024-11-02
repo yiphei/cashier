27945a6e13c7e0fbbe1dab7fab592fce48fd9823
yifei yan <yanyiphei@gmail.com>
2024-11-02 14:15:43 -0700

    added change

--- chat.py
+++ chat.py
@@ -479,7 +479,7 @@ class ChatContext(BaseModel):
                     edge_schemas.extend([(edge, curr_node) for edge in more_edges])
 
 
-    def compute_complete_transition(self, start_edge_schema):
+    def compute_next_edge_schema(self, start_edge_schema):
         def is_prev_completed(edge_schema, is_start_node):
             idx = -1 if is_start_node else -2
             return (
