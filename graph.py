1965a0b3f4b4599da434ede63c128674fa99cdfc
yifei yan <yanyiphei@gmail.com>
2024-11-04 20:56:13 -0800

    refactor

--- graph.py
+++ graph.py
@@ -10,11 +10,6 @@ from function_call_context import StateUpdateError
 from model_tool_decorator import ToolRegistry
 from prompts.cashier_background import CashierBackgroundPrompt
 
-BACKGROUND = (
-    "You are a cashier working for the coffee shop Heaven Coffee. You are physically embedded inside the shop, "
-    "so you will interact with real in-person customers. There is a microphone that transcribes customer's speech to text, "
-    "and a speaker that outputs your text to speech."
-)
 
 
 class Direction(StrEnum):
