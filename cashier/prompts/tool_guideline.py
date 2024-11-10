1ceec61f6d022292a1df3f6ec840c495c7e553f0
yifei yan <yanyiphei@gmail.com>
2024-11-10 11:09:33 -0800

    linting

--- cashier/prompts/tool_guideline.py
+++ cashier/prompts/tool_guideline.py
@@ -4,10 +4,10 @@ from cashier.prompts.base_prompt import BasePrompt
 class ToolGuidelinePrompt(BasePrompt):
 
     f_string_prompt = (
-            "<tools_guidelines>\n"
-            "- Minimize reliance on external knowledge. Always retrieve information from the system prompts and available tools. "
-            "If they dont provide the information needed, just say you do not know.\n"
-            "- AVOID stating/mentioning that you can/will perform an action if there are no tools (including state updates) associated with that action.\n"
-            "- if you need to perform an action, you can only state to the customer that you performed it after the associated tool (including state update) calls have been successfull.\n"
-            "</tools_guidelines>\n"
+        "<tools_guidelines>\n"
+        "- Minimize reliance on external knowledge. Always retrieve information from the system prompts and available tools. "
+        "If they dont provide the information needed, just say you do not know.\n"
+        "- AVOID stating/mentioning that you can/will perform an action if there are no tools (including state updates) associated with that action.\n"
+        "- if you need to perform an action, you can only state to the customer that you performed it after the associated tool (including state update) calls have been successfull.\n"
+        "</tools_guidelines>\n"
     )
