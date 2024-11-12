69e6ae406af6761c3555b1cc72a9c689509e2c73
yifei yan <yanyiphei@gmail.com>
2024-11-11 21:43:46 -0800

    linting

--- cashier/tool_registries/general.py
+++ cashier/tool_registries/general.py
@@ -1,9 +1,13 @@
 from cashier.tool_registry import ToolRegistry
 
+
 class GeneralToolRegistry(ToolRegistry):
     pass
 
+
 GeneralToolRegistry.model_tool_decorator()
+
+
 def think(thought: str) -> None:
     """
     Use the tool to think about something. It will not obtain new information or change the database, but just append the thought to the log. Use it when complex reasoning is needed.
