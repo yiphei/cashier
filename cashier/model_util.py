b961dfb9c035ebf178e0037fe20213933a283ac1
yifei yan <yanyiphei@gmail.com>
2024-11-15 14:23:12 -0800

    added changes

--- cashier/model_util.py
+++ cashier/model_util.py
@@ -5,9 +5,9 @@ import random
 import string
 from collections import defaultdict
 from enum import StrEnum
-from typing import Any, Dict, Optional
+from typing import Any, Dict, Optional, cast
 
-from pydantic import BaseModel, model_validator
+from pydantic import BaseModel, model_validator, Field
 
 from cashier.function_call_context import ToolExceptionWrapper
 
@@ -61,24 +61,24 @@ MODEL_PROVIDER_TO_TOOL_CALL_ID_PREFIX = {
 class FunctionCall(BaseModel):
     name: str
     id: str
-    args_json: Optional[str] = None
-    args: Optional[Dict] = None
+    input_args_json: Optional[str] = Field(default=None, alias='args_json')
+    input_args: Optional[Dict] = Field(default=None, alias='args')
 
     @model_validator(mode="after")
     def check_function_args(self) -> FunctionCall:
-        if self.args_json is None and self.args is None:
+        if self.input_args_json is None and self.input_args is None:
             raise ValueError("One of [args_json, args] must be provided")
 
-        if self.args_json is not None and self.args is None:
-            if self.args_json:
-                self.args = json.loads(self.args_json)
+        if self.input_args_json is not None and self.input_args is None:
+            if self.input_args_json:
+                self.input_args = json.loads(self.input_args_json)
             else:
                 # This case always happens when claude models call inexistent functions.
                 # We still want to construct the function call and let it error downstream.
-                self.args = {}
-                self.args_json = "{}"
-        if self.args is not None and self.args_json is None:
-            self.args_json = json.dumps(self.args)
+                self.input_args = {}
+                self.input_args_json = "{}"
+        if self.input_args is not None and self.input_args_json is None:
+            self.input_args_json = json.dumps(self.input_args)
         return self
 
     @classmethod
@@ -98,3 +98,12 @@ class FunctionCall(BaseModel):
             args_json=args_json,
             args=args,
         )
+
+    @property
+    def args_json(self)-> str:
+        
+        return cast(str, self.input_args_json)
+    
+    @property
+    def args(self)-> Dict:
+        return cast(Dict, self.input_args_json)
