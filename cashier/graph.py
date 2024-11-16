40f69d16d9de1138693d4db159455df3cad23e19
yifei yan <yanyiphei@gmail.com>
2024-11-15 16:39:51 -0800

    more typing

--- cashier/graph.py
+++ cashier/graph.py
@@ -178,11 +178,11 @@ class Node:
                 FwdStateInit if direction == Direction.FWD else BwdStateInit
             )
 
-            if state_init_val == state_init_enum_cls.RESET:
+            if state_init_val == state_init_enum_cls.RESET: # type: ignore
                 return state_pydantic_model()
-            elif state_init_val == state_init_enum_cls.RESUME or (
+            elif state_init_val == state_init_enum_cls.RESUME or ( # type: ignore
                 direction == Direction.FWD
-                and state_init_val == state_init_enum_cls.RESUME_IF_INPUT_UNCHANGED
+                and state_init_val == state_init_enum_cls.RESUME_IF_INPUT_UNCHANGED # type: ignore
                 and input == prev_node.input
             ):
                 return prev_node.state.copy_resume()
