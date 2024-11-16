f69d5e850f51db1083aa6144888dccf4720d5218
yifei yan <yanyiphei@gmail.com>
2024-11-15 16:28:48 -0800

    linting

--- cashier/graph.py
+++ cashier/graph.py
@@ -334,9 +334,9 @@ class BaseStateModel(BaseModel):
         # Iterate through fields and reset those marked as resettable
         for field_name, field_info in self.model_fields.items():
             # Check if field has the resettable marker in its metadata
-            if field_info.json_schema_extra and field_info.json_schema_extra.get( # type: ignore
+            if field_info.json_schema_extra and field_info.json_schema_extra.get(
                 "resettable"
-            ):
+            ):  # type: ignore
                 new_data[field_name] = field_info.default
 
         return self.__class__(**new_data)
