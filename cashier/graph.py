c345207b75d74aeb83626212a3e20a2e86bb5a6a
yifei yan <yanyiphei@gmail.com>
2024-11-15 16:28:41 -0800

    more typing

--- cashier/graph.py
+++ cashier/graph.py
@@ -316,6 +316,8 @@ class EdgeSchema:
                     from_node,
                     to_node,
                 )
+        else:
+            return False, None
 
 
 class Edge(NamedTuple):
@@ -332,7 +334,7 @@ class BaseStateModel(BaseModel):
         # Iterate through fields and reset those marked as resettable
         for field_name, field_info in self.model_fields.items():
             # Check if field has the resettable marker in its metadata
-            if field_info.json_schema_extra and field_info.json_schema_extra.get(
+            if field_info.json_schema_extra and field_info.json_schema_extra.get( # type: ignore
                 "resettable"
             ):
                 new_data[field_name] = field_info.default
@@ -418,7 +420,7 @@ class Graph(BaseModel):
 
     def get_last_edge_schema_by_from_node_schema_id(
         self, node_schema_id: int
-    ) -> EdgeSchema:
+    ) -> Optional[EdgeSchema]:
         edge_schema_id = self.from_node_schema_id_to_last_edge_schema_id[node_schema_id]
         return (
             self.graph_schema.edge_schema_id_to_edge_schema[edge_schema_id]
