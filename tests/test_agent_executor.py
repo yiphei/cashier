11833231b22c622c33d8603ae5f439b803688e2e
yifei yan <yanyiphei@gmail.com>
2024-11-16 21:06:17 -0800

    linting

--- tests/test_agent_executor.py
+++ tests/test_agent_executor.py
@@ -1,24 +1,26 @@
 from io import StringIO
 from unittest.mock import Mock, patch
 
+import pytest
+
 from cashier.agent_executor import AgentExecutor
+from cashier.graph import Node
 from cashier.graph_data.cashier import cashier_graph_schema
 from cashier.model import Model
 from cashier.model_turn import NodeSystemTurn
 from cashier.model_util import ModelProvider
-import pytest
-from cashier.graph import Node
+
 
 class TestAgent:
     @pytest.fixture(autouse=True)
     def setup(self):
         self.model = Mock(spec=Model)
-        self.stdout_patcher = patch('sys.stdout', new_callable=StringIO)
+        self.stdout_patcher = patch("sys.stdout", new_callable=StringIO)
         self.stdout_patcher.start()
         Node._counter = 0
-        
+
         yield
-        
+
         self.stdout_patcher.stop()
         self.model.reset_mock()
 
@@ -31,7 +33,7 @@ class TestAgent:
             remove_prev_tool_calls=remove_prev_tool_calls,
             model_provider=model_provider,
         )
-    
+
     @pytest.fixture
     def agent_executor(self, model_provider, remove_prev_tool_calls):
         return AgentExecutor(
@@ -43,22 +45,18 @@ class TestAgent:
             model_provider=model_provider,
         )
 
-    @pytest.mark.parametrize("model_provider", [
-        ModelProvider.ANTHROPIC, 
-        ModelProvider.OPENAI
-    ])
-    @pytest.mark.parametrize("remove_prev_tool_calls", [
-        True,
-        False
-    ])
+    @pytest.mark.parametrize(
+        "model_provider", [ModelProvider.ANTHROPIC, ModelProvider.OPENAI]
+    )
+    @pytest.mark.parametrize("remove_prev_tool_calls", [True, False])
     def test_initial_node(self, agent_executor):
         start_node_schema = cashier_graph_schema.start_node_schema
         FIRST_NODE = NodeSystemTurn(
             msg_content=start_node_schema.node_system_prompt(
                 node_prompt=cashier_graph_schema.start_node_schema.node_prompt,
-                input= None,
+                input=None,
                 node_input_json_schema=None,
-                state_json_schema= start_node_schema.state_pydantic_model.model_json_schema(),
+                state_json_schema=start_node_schema.state_pydantic_model.model_json_schema(),
                 last_msg=None,
             ),
             node_id=1,
