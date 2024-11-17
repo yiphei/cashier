1871bc374819daa4c76b16d30c91812032fcc8cb
yifei yan <yanyiphei@gmail.com>
2024-11-16 20:09:48 -0800

    linting

--- tests/test_agent_executor.py
+++ tests/test_agent_executor.py
@@ -1,9 +1,11 @@
 import unittest
+
 from cashier.agent_executor import AgentExecutor
-from cashier.model import Model
 from cashier.graph_data.cashier import cashier_graph_schema
-from cashier.model_util import ModelProvider
+from cashier.model import Model
 from cashier.model_turn import NodeSystemTurn
+from cashier.model_util import ModelProvider
+
 
 class Agent(unittest.TestCase):
     def setUp(self):
@@ -12,18 +14,21 @@ class Agent(unittest.TestCase):
 
     def _init_agent_executor(self, remove_prev_tool_calls, model_provider):
         return AgentExecutor(
-            model= self.model,
+            model=self.model,
             elevenlabs_client=None,
             graph_schema=cashier_graph_schema,
             audio_output=False,
             remove_prev_tool_calls=remove_prev_tool_calls,
-            model_provider= model_provider,
+            model_provider=model_provider,
         )
-    
+
     def test_initial_node(self):
         AE = self._init_agent_executor(True, ModelProvider.ANTHROPIC)
 
-        FIRST_NODE = NodeSystemTurn(msg_content=cashier_graph_schema.start_node_schema.node_system_prompt(), node_id=1)
+        FIRST_NODE = NodeSystemTurn(
+            msg_content=cashier_graph_schema.start_node_schema.node_system_prompt(),
+            node_id=1,
+        )
         SECOND_NODE = cashier_graph_schema.start_node_schema.first_turn
 
         self.assertEqual(AE.TC, [FIRST_NODE, SECOND_NODE])
