9907958256a3675f7fcaa7eeb26747f3aa631392
yifei yan <yanyiphei@gmail.com>
2024-11-21 14:44:01 -0800

    added changes

--- cashier/graph/request_graph.py
+++ cashier/graph/request_graph.py
@@ -1,6 +1,9 @@
+from cashier.prompt_action.graph_selection_action import GraphSelectionAction
+
 class RequestGraph:
     def __init__(self, graph_schemas):
         self.graph_schemas = graph_schemas
 
-    def run(self, model_name):
-        pass
+    def get_graph_schemas(self, request):
+        graph_schema_ids = GraphSelectionAction.run("claude-3.5", request, self.graph_schemas)
+        return [graph_schema for graph_schema in self.graph_schemas if graph_schema.id in graph_schema_ids]
