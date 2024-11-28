d7fe396f41c28c3818bb7312e142230d2363d755
yifei yan <yanyiphei@gmail.com>
2024-11-27 22:33:44 -0800

    linting

--- cashier/graph/new_classes.py
+++ cashier/graph/new_classes.py
@@ -1,28 +1,34 @@
-from pydantic import BaseModel
 from typing import List, Optional, Type, Union
+
+from openai.types.chat.chat_completion_tool_param import ChatCompletionToolParam
+from pydantic import BaseModel
+
 from cashier.graph.state_model import BaseStateModel
 from cashier.model.model_turn import ModelTurn
 from cashier.prompts.node_system import NodeSystemPrompt
 from cashier.tool.tool_registry import ToolRegistry
-from openai.types.chat.chat_completion_tool_param import ChatCompletionToolParam
+
 
 class StateMixin:
     def __init__(self, state_pydantic_model: Optional[Type[BaseStateModel]]):
         self.state_pydantic_model = state_pydantic_model
-    
+
+
 class StateWithToolRegistryMixin(StateMixin):
 
-    def __init__(self, 
-            node_prompt: str,
+    def __init__(
+        self,
+        node_prompt: str,
         node_system_prompt: Type[NodeSystemPrompt],
-tate_pydantic_model: Optional[Type[BaseStateModel]], tool_registry_or_tool_defs: Optional[
+        tate_pydantic_model: Optional[Type[BaseStateModel]],
+        tool_registry_or_tool_defs: Optional[
             Union[ToolRegistry, List[ChatCompletionToolParam]]
         ] = None,
-    tool_names: Optional[List[str]] = None,
-    state_pydantic_model: Optional[Type[BaseStateModel]] = None,
-            first_turn: Optional[ModelTurn] = None,
+        tool_names: Optional[List[str]] = None,
+        state_pydantic_model: Optional[Type[BaseStateModel]] = None,
+        first_turn: Optional[ModelTurn] = None,
         run_assistant_turn_before_transition: bool = False,
-        input_pydantic_model: Optional[Type[BaseModel]]=None,
+        input_pydantic_model: Optional[Type[BaseModel]] = None,
     ):
         super().__init__(state_pydantic_model)
         self.node_prompt = node_prompt
