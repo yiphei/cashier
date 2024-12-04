3d560510a29ef0e799e430de3815009b4a420492
yifei yan <yanyiphei@gmail.com>
2024-12-03 23:01:26 -0800

    linting

--- cashier/graph/node_schema.py
+++ cashier/graph/node_schema.py
@@ -2,10 +2,7 @@ from __future__ import annotations
 
 from cashier.graph.auto_mixin_init import AutoMixinInit
 from cashier.graph.has_id_mixin import HasIdMixin
-from cashier.graph.new_classes import (
-    HasActionableMixin,
-    HasActionableSchemaMixin,
-)
+from cashier.graph.new_classes import HasActionableMixin, HasActionableSchemaMixin
 
 
 class Node(HasIdMixin, HasActionableMixin, metaclass=AutoMixinInit):
