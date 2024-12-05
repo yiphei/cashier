552c8fc951e6aae967a13deb10345f0afa4d2c19
yifei yan <yanyiphei@gmail.com>
2024-12-05 11:49:44 -0800

    added changes

--- cashier/graph/node_schema.py
+++ cashier/graph/node_schema.py
@@ -1,10 +1,12 @@
 from __future__ import annotations
+from enum import StrEnum
 from typing import Any, List, Literal, Optional, Type, Union, cast, overload
 
 from pydantic import BaseModel
 
 from cashier.graph.edge_schema import EdgeSchema
 from cashier.graph.mixin.auto_mixin_init import AutoMixinInit
+from cashier.graph.mixin.base_edge_schema import BwdStateInit, FwdStateInit
 from cashier.graph.mixin.has_chat_mixin import Direction, HasChatMixin, HasChatSchemaMixin
 from cashier.graph.mixin.has_id_mixin import HasIdMixin
 from cashier.graph.mixin.state_mixin import BaseStateModel, HasStateSchemaMixin
@@ -12,11 +14,85 @@ from cashier.model.model_turn import ModelTurn
 from cashier.prompts.node_system import NodeSystemPrompt
 from openai.types.chat.chat_completion_tool_param import ChatCompletionToolParam
 
+from cashier.tool.function_call_context import StateUpdateError
 from cashier.tool.tool_registry import ToolRegistry
 
 
-class Node(HasIdMixin, HasChatMixin, metaclass=AutoMixinInit):
-    pass
+class Node(HasIdMixin, metaclass=AutoMixinInit):
+    class Status(StrEnum):
+        IN_PROGRESS = "IN_PROGRESS"
+        COMPLETED = "COMPLETED"
+
+    def __init__(
+        self,
+        schema: HasChatSchemaMixin,
+        input: Any,
+        state: BaseStateModel,
+        prompt: str,
+        in_edge_schema: Optional[EdgeSchema],
+        direction: Direction = Direction.FWD,
+    ):
+        self.prompt = prompt
+        self.input = input
+        self.schema = schema
+        self.status = self.Status.IN_PROGRESS
+        self.in_edge_schema = in_edge_schema
+        self.direction = direction
+        self.has_run_assistant_turn_before_transition = False
+        self.state = state
+        self.first_user_message = False
+
+    @classmethod
+    def init_state(
+        cls,
+        state_pydantic_model: Optional[Type[BaseStateModel]],
+        prev_node: Optional[HasChatMixin],
+        edge_schema: Optional[EdgeSchema],
+        direction: Direction,
+        input: Any,
+    ) -> Optional[BaseStateModel]:
+        if state_pydantic_model is None:
+            return None
+
+        if prev_node is not None:
+            state_init_val = getattr(
+                edge_schema,
+                "fwd_state_init" if direction == Direction.FWD else "bwd_state_init",
+            )
+            state_init_enum_cls = (
+                FwdStateInit if direction == Direction.FWD else BwdStateInit
+            )
+
+            if state_init_val == state_init_enum_cls.RESET:  # type: ignore
+                return state_pydantic_model()
+            elif state_init_val == state_init_enum_cls.RESUME or (  # type: ignore
+                direction == Direction.FWD
+                and state_init_val == state_init_enum_cls.RESUME_IF_INPUT_UNCHANGED  # type: ignore
+                and input == prev_node.input
+            ):
+                return prev_node.state.copy_resume()
+
+        return state_pydantic_model()
+
+    def mark_as_completed(self) -> None:
+        self.status = self.Status.COMPLETED
+
+    def update_state(self, **kwargs: Any) -> None:
+        if self.first_user_message:
+            old_state = self.state.model_dump()
+            new_state = old_state | kwargs
+            self.state = self.state.__class__(**new_state)
+        else:
+            raise StateUpdateError(
+                "cannot update any state field until you get the first customer message in the current conversation. Remember, the current conversation starts after <cutoff_msg>"
+            )
+
+    def get_state(self) -> BaseStateModel:
+        return self.state
+
+    def update_first_user_message(self) -> None:
+        self.first_user_message = True
+
 
 
 class NodeSchema(HasIdMixin, HasStateSchemaMixin, metaclass=AutoMixinInit):
