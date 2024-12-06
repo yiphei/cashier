ef839267ce9fc7bd47af39397ad1f71b9c2f0e33
yifei yan <yanyiphei@gmail.com>
2024-12-05 17:06:19 -0800

    linting

--- data/graph/cashier.py
+++ data/graph/cashier.py
@@ -4,9 +4,7 @@ from pydantic import Field
 
 from cashier.graph.edge_schema import EdgeSchema
 from cashier.graph.graph_schema import GraphSchema
-from cashier.graph.mixin.base_edge_schema import (
-    StateTransitionConfig,
-)
+from cashier.graph.mixin.base_edge_schema import StateTransitionConfig
 from cashier.graph.mixin.state_mixin import BaseStateModel
 from cashier.graph.node_schema import NodeSchema
 from cashier.graph.request_graph import RequestGraphSchema
