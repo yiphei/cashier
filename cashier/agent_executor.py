e77c9182bbab49faf6ac036e3deee2b28201dbbb
yifei yan <yanyiphei@gmail.com>
2024-12-05 18:31:03 -0800

    added changes

--- cashier/agent_executor.py
+++ cashier/agent_executor.py
@@ -26,9 +26,9 @@ class AgentExecutor:
         remove_prev_tool_calls: bool,
         request_graph_schema=None,
     ):
-        self.request_graph_schema = request_graph_schema
-        self.request_graph = RequestGraph(None, self.request_graph_schema)
-        self.curr_graph_schema = None
+        # self.request_graph_schema = request_graph_schema
+        # self.request_graph = RequestGraph(None, self.request_graph_schema)
+        # self.curr_graph_schema = None
 
         self.remove_prev_tool_calls = remove_prev_tool_calls
         self.audio_output = audio_output
@@ -37,9 +37,9 @@ class AgentExecutor:
 
         self.need_user_input = True
 
-        self.graph = None
-        self.request_graph.init_node_core(
-            self.request_graph_schema.start_node_schema,
+        self.graph = RequestGraph(None, request_graph_schema)
+        self.graph.init_node_core(
+            request_graph_schema.start_node_schema,
             None,
             None,
             None,
@@ -60,11 +60,9 @@ class AgentExecutor:
         model_provider = (
             model_provider or self.last_model_provider or ModelProvider.OPENAI
         )
-        self.request_graph.handle_user_turn(
+        self.graph.handle_user_turn(
             msg, self.TC, model_provider, self.remove_prev_tool_calls
         )
-        if isinstance(self.request_graph.curr_node, Graph):
-            self.graph = self.request_graph.curr_node
 
     def execute_function_call(
         self, fn_call: FunctionCall, fn_callback: Optional[Callable] = None
@@ -74,13 +72,13 @@ class AgentExecutor:
             f"[FUNCTION_CALL] {Style.BRIGHT}name: {fn_call.name}, id: {fn_call.id}{Style.NORMAL} with args:\n{json.dumps(function_args, indent=4)}"
         )
         with FunctionCallContext() as fn_call_context:
-            if fn_call.name not in self.graph.curr_node.schema.tool_registry.tool_names:
+            if fn_call.name not in self.graph.curr_executable.schema.tool_registry.tool_names:
                 raise InexistentFunctionError(fn_call.name)
 
             if fn_call.name.startswith("get_state"):
-                fn_output = getattr(self.graph.curr_node, fn_call.name)(**function_args)
+                fn_output = getattr(self.graph.curr_executable, fn_call.name)(**function_args)
             elif fn_call.name.startswith("update_state"):
-                fn_output = self.graph.curr_node.update_state(**function_args)  # type: ignore
+                fn_output = self.graph.curr_executable.update_state(**function_args)  # type: ignore
             elif fn_callback is not None:
                 # TODO: this exists for benchmarking. remove this once done
                 fn_output = fn_callback(**function_args)
@@ -90,7 +88,7 @@ class AgentExecutor:
                 ):
                     fn_output = json.loads(fn_output)
             else:
-                fn = self.graph.curr_node.schema.tool_registry.fn_name_to_fn[
+                fn = self.graph.curr_executable.schema.tool_registry.fn_name_to_fn[
                     fn_call.name
                 ]
                 fn_output = fn(**function_args)
@@ -145,7 +143,7 @@ class AgentExecutor:
                     is_completed,
                     fake_fn_call,
                     fake_fn_output,
-                ) = self.request_graph.check_transition(function_call, is_success)
+                ) = self.graph.check_transition(function_call, is_success)
                 if new_node_schema is not None:
                     self.new_edge_schema = new_edge_schema
                     self.new_node_schema = new_node_schema
@@ -157,15 +155,15 @@ class AgentExecutor:
         self.TC.add_assistant_turn(
             model_completion.msg_content,
             model_completion.model_provider,
-            self.graph.curr_node.schema.tool_registry,
+            self.graph.curr_executable.schema.tool_registry,
             fn_calls,
             fn_id_to_output,
         )
         if self.new_edge_schema and (
-            not self.graph.curr_node.schema.run_assistant_turn_before_transition
-            or self.graph.curr_node.has_run_assistant_turn_before_transition
+            not self.graph.curr_executable.schema.run_assistant_turn_before_transition
+            or self.graph.curr_executable.has_run_assistant_turn_before_transition
         ):
-            self.request_graph.init_next_node(
+            self.graph.init_next_node(
                 self.new_node_schema,
                 self.new_edge_schema,
                 self.TC,
@@ -178,18 +176,18 @@ class AgentExecutor:
     def get_model_completion_kwargs(self) -> Dict[str, Any]:
         force_tool_choice = self.force_tool_choice
         self.force_tool_choice = None
-        target_graph = self.graph or self.request_graph or None
+        target_graph = self.graph.curr_executable
         return {
             "turn_container": self.TC,
             "tool_registry": (
-                target_graph.curr_node.schema.tool_registry
+                target_graph.schema.tool_registry
                 if target_graph is not None
                 else None
             ),
             "force_tool_choice": force_tool_choice,
             "exclude_update_state_fns": (
-                not target_graph.curr_node.first_user_message
-                if target_graph.curr_node is not None
+                not target_graph.first_user_message
+                if target_graph is not None
                 else False
             ),
         }
