6ffb782dae63fec2fde84474c4fc06ebb54266a8
yifei yan <yanyiphei@gmail.com>
2024-12-09 21:59:44 -0800

    linting

--- cashier/graph/graph_schema.py
+++ cashier/graph/graph_schema.py
@@ -222,7 +222,8 @@ class Graph(BaseGraph):
     def check_self_completion(self, fn_call, is_fn_call_success):
         assert self.schema.completion_config is not None
         self_completion = (
-            self.curr_node.schema == self.schema.last_node_schema and self.schema.completion_config.run_check(
+            self.curr_node.schema == self.schema.last_node_schema
+            and self.schema.completion_config.run_check(
                 self.state, fn_call, is_fn_call_success
             )
         )
