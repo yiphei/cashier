6773a70b08f5949252f234e941bc0377bd7e31d8
yifei yan <yanyiphei@gmail.com>
2024-12-12 14:10:48 -0800

    added changes

--- cashier/graph/base/base_terminable_graph.py
+++ cashier/graph/base/base_terminable_graph.py
@@ -190,18 +190,6 @@ class BaseTerminableGraph(BaseGraph):
                     )
         self.curr_node.update_first_user_message()
 
-    def check_self_completion(self, fn_call, is_fn_call_success):
-        assert self.schema.completion_config is not None
-        self_completion = (
-            self.curr_node.schema == self.schema.last_node_schema
-            and self.schema.completion_config.run_check(
-                self.state, fn_call, is_fn_call_success
-            )
-        )
-        if self_completion:
-            self.mark_as_internally_completed()
-        return self_completion
-
     def init_conversation_core(
         self,
         node_schema: ConversationNodeSchema,
@@ -230,3 +218,15 @@ class BaseTerminableGraph(BaseGraph):
 
     def get_next_edge_schema(self):
         return self.next_edge_schemas
+    
+    def check_self_completion(self, fn_call, is_fn_call_success):
+        assert self.schema.completion_config is not None
+        self_completion = (
+            self.curr_node.schema == self.schema.last_node_schema
+            and self.schema.completion_config.run_check(
+                self.state, fn_call, is_fn_call_success
+            )
+        )
+        if self_completion:
+            self.mark_as_internally_completed()
+        return self_completion
