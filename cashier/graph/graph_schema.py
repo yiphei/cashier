d5f77b1e63d63c69cdd54d1362753024391156c6
yifei yan <yanyiphei@gmail.com>
2024-12-19 15:04:25 -0800

    added changes

--- cashier/graph/graph_schema.py
+++ cashier/graph/graph_schema.py
@@ -66,25 +66,23 @@ class Graph(BaseTerminableGraph):
         from cashier.graph.and_graph_schema import ANDGraphSchema
 
         node_schema = self.schema.start_node_schema
-        edge_schema = None
+        target_edge_schema = None
         next_edge_schema = self.from_node_schema_id_to_edge_schema[node_schema.id]
-        passed_check = True
-        while passed_check:
-            passed_check = False
-            if next_edge_schema.check_transition_config(
+        while next_edge_schema:
+            edge_schema = next_edge_schema
+            next_edge_schema = None
+            if edge_schema.check_transition_config(
                 self.state,
                 None,
                 None,
                 check_resettable_fields=False,
             ) and not isinstance(
-                next_edge_schema.from_node_schema, ANDGraphSchema
+                edge_schema.from_node_schema, ANDGraphSchema
             ):  # TODO: fix this
-                passed_check = True
-                node_schema = next_edge_schema.to_node_schema
-                edge_schema = next_edge_schema
+                node_schema = edge_schema.to_node_schema
+                target_edge_schema = edge_schema
                 next_edge_schema = self.schema.from_node_schema_id_to_edge_schema.get(
                     node_schema.id, None
                 )
-                break
 
-        return node_schema, edge_schema
+        return node_schema, target_edge_schema
