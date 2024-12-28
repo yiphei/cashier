a8f913c6edf857377f3b0375e00441cb9477ee54
yifei yan <yanyiphei@gmail.com>
2024-12-28 10:38:24 -0800

    added changes

--- cashier/graph/base/base_graph.py
+++ cashier/graph/base/base_graph.py
@@ -458,7 +458,7 @@ class BaseGraph(BaseGraphExecutable, HasIdMixin):
         return need_user_input
 
 
-def get_fn_names_fixture(conv_node_schema):
+def get_fn_names_fixture(conv_node_schema, exclude_update_fn = False):
     update_state_fn_names = []
     non_state_fn_names = []
     for tool_name in conv_node_schema.tool_registry.openai_tool_name_to_tool_def.keys():
@@ -477,19 +477,23 @@ def get_fn_names_fixture(conv_node_schema):
     one_non_state_fn_name = non_state_fn_names[0] if non_state_fn_names else None
 
     fn_names_fixture = [[inexistent_fn_name]]
-    if one_update_state_fn_name:
+    if one_update_state_fn_name and not exclude_update_fn:
         fn_names_fixture.append([one_update_state_fn_name])
     if get_state_fn_name:
         fn_names_fixture.append([get_state_fn_name])
+        fn_names_fixture.append([get_state_fn_name, inexistent_fn_name])
     if one_non_state_fn_name:
         fn_names_fixture.append([one_non_state_fn_name])
         fn_names_fixture.append([one_non_state_fn_name, one_non_state_fn_name])
-    if get_state_fn_name and one_update_state_fn_name:
+    if get_state_fn_name and one_non_state_fn_name:
+        fn_names_fixture.append([get_state_fn_name, one_non_state_fn_name])
+        fn_names_fixture.append([get_state_fn_name, one_non_state_fn_name, one_non_state_fn_name])
+    if get_state_fn_name and one_update_state_fn_name and not exclude_update_fn:
         fn_names_fixture.append([get_state_fn_name, one_update_state_fn_name])
         fn_names_fixture.append(
             [get_state_fn_name, one_update_state_fn_name, inexistent_fn_name]
         )
-    if get_state_fn_name and one_update_state_fn_name and one_non_state_fn_name:
+    if get_state_fn_name and one_update_state_fn_name and one_non_state_fn_name and not exclude_update_fn:
         fn_names_fixture.append(
             [get_state_fn_name, one_non_state_fn_name, one_update_state_fn_name]
         )
