83a88039da58cbaf9a9ddf24e6d32625d86a21c4
yifei yan <yanyiphei@gmail.com>
2024-12-29 15:00:00 -0800

    added changes

--- tests/base_test.py
+++ tests/base_test.py
@@ -453,7 +453,7 @@ class BaseTest:
         return request.param
 
     @pytest.fixture(params=[True, False], autouse=True)
-    def is_stream(self, base_setup, request):
+    def is_stream(self, base_setup, request): # TODO: this fixture can prob be deleted. Streaming should not affect test integrity
         self.fixtures.is_stream = request.param
         return request.param
 
