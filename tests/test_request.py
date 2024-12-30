e2e84e0c85b358e2e0f3e772d1e1255ce4a3b7ef
yifei yan <yanyiphei@gmail.com>
2024-12-30 14:47:13 -0800

    linting

--- tests/test_request.py
+++ tests/test_request.py
@@ -355,30 +355,6 @@ class TestRequest(BaseTest):
         )
         self.add_messages_from_turn(t5aaa)
 
-        # agent_executor.graph.requests.append("change baggage")
-        # agent_executor.graph.graph_schema_sequence.append(CHANGE_BAGGAGE_GRAPH_SCHEMA)
-        # agent_executor.graph.graph_schema_id_to_task[CHANGE_BAGGAGE_GRAPH_SCHEMA.id] = (
-        #     "change baggage"
-        # )
-
-        # --------------------------------
-
-        # fake_fn_call = self.recreate_fake_single_fn_call(
-        #     "think",
-        #     {
-        #         "thought": "I just completed the current request. The next request to be addressed is: change baggage. I must explicitly inform the customer that the current request is completed and that I will address the next request right away. Only after I informed the customer do I receive the tools to address the next request."
-        #     },
-        # )
-
-        # t6aa = AssistantTurn(
-        #     msg_content=None,
-        #     model_provider=self.fixtures.model_provider,
-        #     tool_registry=self.fixtures.agent_executor.graph.curr_conversation_node.schema.tool_registry,
-        #     fn_calls=[fake_fn_call],
-        #     fn_call_id_to_fn_output={fake_fn_call.id: None},
-        # )
-        # self.add_messages_from_turn(t6aa)
-
         t6 = self.add_assistant_turn(
             "finished task",
         )
