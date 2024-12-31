df5fa5e168e6f638edd6796a5c2713b79bfd386b
yifei yan <yanyiphei@gmail.com>
2024-12-31 15:55:43 -0800

    added changes

--- tests/test_and_graph.py
+++ tests/test_and_graph.py
@@ -163,7 +163,7 @@ class TestAndGraph(BaseTest):
         )
 
     @pytest.mark.parametrize(
-        "fn_names", get_fn_names_fixture(normal_get_user_id_node_schema)
+        "fn_names", get_fn_names_fixture(get_user_id_node_schema)
     )
     @pytest.mark.parametrize("separate_fn_calls", [True, False])
     def test_add_assistant_turn_with_tool_calls(
@@ -191,7 +191,7 @@ class TestAndGraph(BaseTest):
 
     @pytest.mark.parametrize(
         "fn_names",
-        get_fn_names_fixture(normal_get_user_id_node_schema, exclude_update_fn=True)
+        get_fn_names_fixture(get_user_id_node_schema, exclude_update_fn=True)
         + [[]],
     )
     def test_state_update_before_user_turn(
