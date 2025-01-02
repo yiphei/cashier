05b4c3ffbcae8dfb0f9f2c63546d9165bf98073d
yifei yan <yanyiphei@gmail.com>
2025-01-01 23:34:17 -0800

    linting

--- cashier/graph/base/base_graph.py
+++ cashier/graph/base/base_graph.py
@@ -422,7 +422,9 @@ class BaseGraph(BaseGraphExecutable, HasIdMixin):
                 )
                 fn_calls.append(function_call)
                 alert = self.curr_conversation_node.handle_alerts(
-                    function_call.name, self.curr_conversation_node.state, self.curr_conversation_node.input
+                    function_call.name,
+                    self.curr_conversation_node.state,
+                    self.curr_conversation_node.input,
                 )
                 if alert is not None:
                     fake_fn_call = create_think_fn_call(alert)
