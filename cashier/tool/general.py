00643929b007e54f1f0c6a28e7b332cfbedfcd4b
yifei yan <yanyiphei@gmail.com>
2025-01-03 15:35:59 -0800

    added changes

--- cashier/tool/general.py
+++ cashier/tool/general.py
@@ -16,3 +16,16 @@ def think(thought: str) -> None:
 
     """
     return None
+
+
+@GeneralToolRegistry.model_tool_decorator()
+def think_deep(thought: str) -> None:
+    """
+    Use the tool to think very deeply about something very important and difficult. This is a pure function (i.e. has no side effect).
+
+    Args:
+        thought: A long and deep thought to think about. There is no limit on the length of the thought, so longer and deeper thoughts are better.
+
+
+    """
+    return None
